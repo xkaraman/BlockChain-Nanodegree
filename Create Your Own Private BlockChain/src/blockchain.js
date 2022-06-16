23a9d25923d5058fceb306c7ae30485ab1b42c6d
Xenofon Karamanos <xenakiskaramanos@gmail.com>
2022-06-17 01:19:41 +0300

    Use find instead of filter

--- Create Your Own Private BlockChain/src/blockchain.js
+++ Create Your Own Private BlockChain/src/blockchain.js
@@ -149,7 +149,7 @@ class Blockchain {
     getBlockByHash(hash) {
         let self = this;
         return new Promise((resolve, reject) => {
-            let block = self.chain.filter(p => p.hash === hash)[0];
+            let block = self.chain.find(p => p.hash === hash);
             block ? resolve(block) : resolve(null);
         });
     }
@@ -162,7 +162,7 @@ class Blockchain {
     getBlockByHeight(height) {
         let self = this;
         return new Promise((resolve, reject) => {
-            let block = self.chain.filter(p => p.height === height)[0];
+            let block = self.chain.find(p => p.height === height);
             if(block){
                 resolve(block);
             } else {
