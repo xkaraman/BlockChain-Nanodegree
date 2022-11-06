1928006453c921630758a359e39cc727cae9919f
Xenofon Karamanos <xenakiskaramanos@gmail.com>
2022-11-06 17:43:48 +0200

    Update server

--- FlightSurety/src/server/index.js
+++ FlightSurety/src/server/index.js
@@ -4,12 +4,19 @@ import app from './server'
 
 const server = http.createServer(app)
 let currentApp = app
-server.listen(3000)
+server.listen(3000, async () => {
+    console.log('Oracle Server App running on port 3000...');
+    console.log('GET /logs for server log history');
+    console.log('GET /oracles for registered oracles');
+    console.log('-----------------------------------------');
+    console.log('v               LOGS                    v');
+    console.log('-----------------------------------------');
+})
 
 if (module.hot) {
- module.hot.accept('./server', () => {
-  server.removeListener('request', currentApp)
-  server.on('request', app)
-  currentApp = app
- })
+    module.hot.accept('./server', () => {
+        server.removeListener('request', currentApp)
+        server.on('request', app)
+        currentApp = app
+    })
 }
--- FlightSurety/src/server/server.js
+++ FlightSurety/src/server/server.js
@@ -7,22 +7,123 @@ import express from 'express';
 let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
-let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+let fsApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+
+const oracleEventHistory = [];
+const oracles = [];//config.oracles;
+const MIN_ORACLES = 20
+const FEE = "1";
+const GAS = 6721975
+
+function logEvent(event) {
+  oracleEventHistory.push(event)
+  console.log(event);
+}
+
+function addOracle(oracle) {
+  oracles.push(oracle);
+}
+
+async function registerOracles(accounts) {
+  logEvent("Started Registering Oracles")
+  for (let i = 0; i < accounts.length; i++) {
+    const oracle = accounts[i];
+    const feeEth = web3.utils.toWei(FEE);
+
+    logEvent(`Attemp to Reg Oracle : ${oracle}`);
+    await fsApp.methods.registerOracle().send({ from: oracle, value: feeEth, gas: GAS })
+      .then(async receipt => {
+        addOracle(oracle);
+        let result = await fsApp.methods.getMyIndexes().call({ from: oracle });
+        logEvent(`Registerd Succesfully: ${oracle} Indexes: ${result[0]}, ${result[1]}, ${result[2]}`)
+      })
+      .catch(err => {
+        console.log(err)
+        logEvent(`Failed to Reg Oracle : ${oracle}`)
+      })
+  }
+  logEvent(`Finished Registering Oracles`)
+}
+
+function generateRandomStatus() {
+  const STATUS_CODE_UNKNOWN = 0;
+  const STATUS_CODE_ON_TIME = 10; // "STATUS_CODE_ON_TIME";
+  const STATUS_CODE_LATE_AIRLINE = 20; //"STATUS_CODE_LATE_AIRLINE";
+  const STATUS_CODE_LATE_WEATHER = 30; //"STATUS_CODE_LATE_WEATHER";
+  const STATUS_CODE_LATE_TECHNICAL = 40; //"STATUS_CODE_LATE_TECHNICAL";
+  const STATUS_CODE_LATE_OTHER = 50;//"STATUS_CODE_LATE_OTHER";
+
+  const statuses = [STATUS_CODE_LATE_AIRLINE];
+  return statuses[Math.floor(Math.random() * statuses.length)];
+}
+
+async function respondToOracleRequest(oracleIndex, airlineAddress, flightName, flightTimestamp) {
+  logEvent(`Responding to Oracle Request [Index: ${oracleIndex}],[Flight Name: ${flightName}]`)
+
+  oracles.forEach(async (oracle) => {
+    const flightStatus = generateRandomStatus();
+    await fsApp.methods.submitOracleResponse(oracleIndex, airlineAddress, flightName, flightTimestamp, flightStatus)
+      .send({ from: oracle, gas: GAS })
+      .then(receipt => {
+        logEvent(`Oracle Response: ${flightName}, ${flightStatus} `)
+      })
+      .catch(err => {
+        logEvent(`Oracle ${oracle} ${err}`)
+      })
+  });
+
+  logEvent(`Respond Complete`)
+}
 
 
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
-});
+async function startOracles() {
+  const accounts = await web3.eth.getAccounts()
+  const oracles = accounts.slice(-(MIN_ORACLES + 1))
+  // console.log(oracles)
+
+  await registerOracles(oracles);
+
+  await fsApp.events.OracleRequest({})
+    .on('data', async (event) => {
+      console.log("Event list:", event.returnValues.timestamp, "=======")
+      // console.log(event.returnValues, "=======")
+      const { index, airline, flight, timestamp } = event.returnValues;
+      console.log(index, airline, flight, timestamp)
+      logEvent(`OracleRequest Event fired: ${event}`)
+      await respondToOracleRequest(index, airline, flight, timestamp);
+    })
+    .on('error', error => {
+      logEvent(error)
+    })
+
+  await fsApp.events.FlightStatusInfo({ fromBlock: "pending" })
+    .on('data', event => {
+      logEvent(`FlightStatusInfo Event fired: ${event.blockNumber}`)
+    })
+    .on('error', error => {
+      logEvent(error)
+    })
+}
+
 
 const app = express();
 app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
+  res.send({
+    message: 'An API for use with your Dapp!'
+  })
 })
+
+app.get('/logs', (req, res) => {
+  res.json({ oracleEventHistory });
+})
+
+app.get('/oracles', (req, res) => {
+  res.json({ oracles });
+})
+
+app.set('json spaces', 2)
+
+startOracles();
 
 export default app;
 
